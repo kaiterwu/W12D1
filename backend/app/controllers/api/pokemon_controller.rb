4809e76700709bf1d3dae130047c498253c03d2b
Kaiter Wu <wukaiter@gmail.com>
2023-03-27 16:58:11 -0400

    jbuilder start

---
+++ backend/app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,34 @@
+class Api::PokemonController < ApplicationController
+    def types
+        render json: [
+            "fire",
+            "electric",
+            "normal",
+            "ghost",
+            "psychic",
+            "water",
+            "bug",
+            "dragon",
+            "grass",
+            "fighting",
+            "ice",
+            "flying",
+            "poison",
+            "ground",
+            "rock",
+            "steel"
+          ]
+          
+    end 
+
+    def index 
+        @pokemons = Pokemon.all 
+        render :index
+    end 
+
+    def show 
+        @pokemon = Pokmond.find(params[:id])
+    end 
+
+    
+end
