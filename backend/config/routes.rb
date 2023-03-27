e80135cfbd5b55c8e9d4864b7982e1c1f8937a57
Yong <yong.lin13@gmail>
2023-03-27 16:38:21 -0400

    finished routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: { format: :json } do
+    get '/pokemon/types', to: 'pokemon#types', as: 'pokemon_types'
+    resources :items, only: [:update, :destroy]
+    resources :pokemon, only: [:index, :create, :show, :update] do 
+      resources :items, only: [:index, :create]
+    end
+  end
 end
